e5cdb9778cd7c6d6d9db2ab77654fedbacec995b
mashkoor098 <60177001+mashkoor098@users.noreply.github.com>
2024-02-28 17:20:12 +0530

    Delete day_1 directory

--- day_1/Lecture_16.py
+++
@@ -1,8 +0,0 @@
-#Generate the brand name using the combination of city name & pet name.
-
-print("Welcome to the Band Name Generator.")
-
-city_name = input("What's the name of the city you grew up in?\n")
-pet_name = input("What's your pet's name?\n")
-
-print("Your band name could be '"+city_name+" "+pet_name+"'")
